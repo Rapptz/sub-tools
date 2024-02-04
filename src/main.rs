c98f10dee034cba54db13a07526e8c23cde1f918
Rapptz <rapptz@gmail.com>
2024-02-04 05:29:12 -0500

    Add support for fixing Japanese subtitles

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     fs::File,
     io::{Read, Seek, Write},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
 use anyhow::Context;
@@ -16,6 +16,19 @@ struct Args {
     /// Shift the times of the subtitles by the given seconds
     #[arg(long, value_parser = valid_duration, allow_negative_numbers = true)]
     shift: Option<f32>,
+    /// Fixes common issues with Japanese subtitle files.
+    ///
+    /// The things removed are as follows:
+    /// * [外:37F6ECF37A0A3EF8DFF083CCC8754F81]-like instances of text
+    /// * Half-width kana is converted into full width kana
+    #[arg(long = "fix-jp", required = false, default_value_t = false)]
+    fix_japanese: bool,
+}
+
+impl Args {
+    fn is_modified(&self) -> bool {
+        self.fix_japanese || self.shift.is_some()
+    }
 }
 
 fn valid_duration(s: &str) -> Result<f32, String> {
@@ -29,64 +42,94 @@ fn valid_duration(s: &str) -> Result<f32, String> {
     }
 }
 
-fn shift_subtitle_file(file: PathBuf, shift: f32) -> anyhow::Result<()> {
-    let mut fp = File::open(&file)?;
-    let new_filename = match file.file_stem() {
-        Some(filename) => {
-            let mut new_file = PathBuf::new();
-            let mut filename = filename.to_os_string();
-            filename.push("_shifted.srt");
-            new_file.set_file_name(filename);
-            new_file
+#[derive(Debug)]
+struct InProgressFile {
+    pending_filename: PathBuf,
+    dialogue: Vec<Dialogue>,
+}
+
+impl InProgressFile {
+    fn new(path: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let path = path.as_ref();
+        let mut fp = File::open(path)?;
+        let new_filename = match path.file_stem() {
+            Some(filename) => {
+                let mut new_file = PathBuf::new();
+                let mut filename = filename.to_os_string();
+                filename.push("_modified.srt");
+                new_file.set_file_name(filename);
+                new_file
+            }
+            None => anyhow::bail!("invalid filename given"),
+        };
+
+        let mut buffer = String::new();
+        // Try to check if there's a UTF-8 BOM somewhere
+        let mut bom: [u8; 3] = [0; 3];
+        fp.read_exact(&mut bom)?;
+        if bom != [0xEF, 0xBB, 0xBF] {
+            fp.rewind()?;
         }
-        None => anyhow::bail!("invalid filename given"),
-    };
 
-    let mut buffer = String::new();
-    // Try to check if there's a UTF-8 BOM somewhere
-    let mut bom: [u8; 3] = [0; 3];
-    fp.read_exact(&mut bom)?;
-    if bom != [0xEF, 0xBB, 0xBF] {
-        fp.rewind()?;
+        fp.read_to_string(&mut buffer)?;
+
+        if buffer.contains("\r\n") {
+            buffer = buffer.replace("\r\n", "\n");
+        }
+        let dialogue = buffer
+            .split_terminator("\n\n")
+            .enumerate()
+            .map(|(i, s)| {
+                s.parse::<Dialogue>()
+                    .with_context(|| format!("from srt dialogue {}", i + 1))
+            })
+            .collect::<Result<Vec<_>, _>>()
+            .with_context(|| format!("Failed to extract dialogue from {}", path.display()))?;
+
+        Ok(Self {
+            pending_filename: new_filename,
+            dialogue,
+        })
     }
 
-    fp.read_to_string(&mut buffer)?;
+    fn save(&self) -> anyhow::Result<()> {
+        let mut new_contents = self
+            .dialogue
+            .iter()
+            .map(|f| f.to_string())
+            .collect::<Vec<_>>()
+            .join("\n\n");
 
-    if buffer.contains("\r\n") {
-        buffer = buffer.replace("\r\n", "\n");
+        new_contents.push_str("\n\n");
+        let mut new_fp = File::create(&self.pending_filename)
+            .with_context(|| "could not create new subtitle file".to_string())?;
+        new_fp.write_all(new_contents.as_bytes())?;
+        Ok(())
     }
-
-    let mut dialogue = buffer
-        .split_terminator("\n\n")
-        .enumerate()
-        .map(|(i, s)| {
-            s.parse::<Dialogue>()
-                .with_context(|| format!("from srt dialogue {}", i + 1))
-        })
-        .collect::<Result<Vec<_>, _>>()
-        .with_context(|| format!("Failed to extract dialogue from {}", file.display()))?;
-
-    let mut new_contents = dialogue
-        .iter_mut()
-        .map(|f| {
-            f.shift_by(shift);
-            f.to_string()
-        })
-        .collect::<Vec<_>>()
-        .join("\n\n");
-
-    new_contents.push_str("\n\n");
-    let mut new_fp = File::create(new_filename)
-        .with_context(|| "could not create new subtitle file".to_string())?;
-    new_fp.write_all(new_contents.as_bytes())?;
-    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    let files: anyhow::Result<Vec<_>> = args.files.iter().map(InProgressFile::new).collect();
+    let mut files = files?;
     if let Some(shift) = args.shift {
-        for file in args.files {
-            shift_subtitle_file(file, shift)?;
+        for file in files.iter_mut() {
+            for dialogue in file.dialogue.iter_mut() {
+                dialogue.shift_by(shift);
+            }
+        }
+    }
+
+    if args.fix_japanese {
+        for file in files.iter_mut() {
+            for dialogue in file.dialogue.iter_mut() {
+                dialogue.fix_japanese();
+            }
+        }
+    }
+    if !files.is_empty() && args.is_modified() {
+        for file in files {
+            file.save()?;
         }
     }
     Ok(())
--- src/srt.rs
+++ src/srt.rs
@@ -8,6 +8,118 @@ pub struct Dialogue {
     pub text: String,
 }
 
+#[inline]
+fn is_halfwidth_kana(c: char) -> bool {
+    ('\u{FF65}'..='\u{FF9F}').contains(&c)
+}
+
+#[inline]
+fn is_katakana(c: char) -> bool {
+    ('\u{30A0}'..='\u{30FA}').contains(&c)
+}
+
+fn replace_halfwith_kana(input: &str) -> Option<String> {
+    let index = input.find(is_halfwidth_kana)?;
+    let mut output = String::from(&input[..index]);
+    output.reserve(input.len() - index);
+    let chars = input[index..].chars();
+    for ch in chars {
+        match ch {
+            '･' => output.push('・'),
+            'ｦ' => output.push('ヲ'),
+            'ｧ' => output.push('ァ'),
+            'ｨ' => output.push('ィ'),
+            'ｩ' => output.push('ゥ'),
+            'ｪ' => output.push('ェ'),
+            'ｫ' => output.push('ォ'),
+            'ｬ' => output.push('ャ'),
+            'ｭ' => output.push('ュ'),
+            'ｮ' => output.push('ョ'),
+            'ｯ' => output.push('ッ'),
+            'ｰ' => output.push('ー'),
+            'ｱ' => output.push('ア'),
+            'ｲ' => output.push('イ'),
+            'ｳ' => output.push('ウ'),
+            'ｴ' => output.push('エ'),
+            'ｵ' => output.push('オ'),
+            'ｶ' => output.push('カ'),
+            'ｷ' => output.push('キ'),
+            'ｸ' => output.push('ク'),
+            'ｹ' => output.push('ケ'),
+            'ｺ' => output.push('コ'),
+            'ｻ' => output.push('サ'),
+            'ｼ' => output.push('シ'),
+            'ｽ' => output.push('ス'),
+            'ｾ' => output.push('セ'),
+            'ｿ' => output.push('ソ'),
+            'ﾀ' => output.push('タ'),
+            'ﾁ' => output.push('チ'),
+            'ﾂ' => output.push('ツ'),
+            'ﾃ' => output.push('テ'),
+            'ﾄ' => output.push('ト'),
+            'ﾅ' => output.push('ナ'),
+            'ﾆ' => output.push('ニ'),
+            'ﾇ' => output.push('ヌ'),
+            'ﾈ' => output.push('ネ'),
+            'ﾉ' => output.push('ノ'),
+            'ﾊ' => output.push('ハ'),
+            'ﾋ' => output.push('ヒ'),
+            'ﾌ' => output.push('フ'),
+            'ﾍ' => output.push('ヘ'),
+            'ﾎ' => output.push('ホ'),
+            'ﾏ' => output.push('マ'),
+            'ﾐ' => output.push('ミ'),
+            'ﾑ' => output.push('ム'),
+            'ﾒ' => output.push('メ'),
+            'ﾓ' => output.push('モ'),
+            'ﾔ' => output.push('ヤ'),
+            'ﾕ' => output.push('ユ'),
+            'ﾖ' => output.push('ヨ'),
+            'ﾗ' => output.push('ラ'),
+            'ﾘ' => output.push('リ'),
+            'ﾙ' => output.push('ル'),
+            'ﾚ' => output.push('レ'),
+            'ﾛ' => output.push('ロ'),
+            'ﾜ' => output.push('ワ'),
+            'ﾝ' => output.push('ン'),
+            // These two are a bit of a special case
+            // They might cause some mess ups, but ideally it doesn't
+            'ﾞ' => {
+                // In the katakana table, a voiced dakuten
+                // is merely +1 char from the previous one
+                match output.pop() {
+                    Some(ch) => {
+                        if is_katakana(ch) {
+                            output.push(char::from_u32(ch as u32 + 1).unwrap());
+                        } else {
+                            output.push(ch);
+                            output.push('゛');
+                        }
+                    }
+                    None => output.push('゛'),
+                }
+            }
+            'ﾟ' => {
+                // In the katakana table, a voiced dakuten
+                // is merely +2 char from the previous one
+                match output.pop() {
+                    Some(ch) => {
+                        if is_katakana(ch) {
+                            output.push(char::from_u32(ch as u32 + 2).unwrap());
+                        } else {
+                            output.push(ch);
+                            output.push('゜');
+                        }
+                    }
+                    None => output.push('゜'),
+                }
+            }
+            _ => output.push(ch),
+        }
+    }
+    Some(output)
+}
+
 impl Dialogue {
     pub fn shift_by(&mut self, seconds: f32) {
         let duration = Duration::from_secs_f32(seconds.abs());
@@ -17,6 +129,24 @@ impl Dialogue {
         } else {
             self.start = self.start.saturating_add(duration);
             self.end = self.end.saturating_add(duration);
+        }
+    }
+
+    pub fn fix_japanese(&mut self) {
+        // Remove e.g. [外:37F6ECF37A0A3EF8DFF083CCC8754F81]-like instances of text
+        if let Some(index) = self.text.find("[外:") {
+            let sub = &self.text[index..];
+            // Find the first instance that's not uppercase hex
+            if let Some(end) = sub.find(|c: char| !c.is_ascii_hexdigit()) {
+                if sub.as_bytes()[end] == b']' {
+                    self.text.drain(index..=end);
+                }
+            }
+        }
+
+        // Fix up half-width kana
+        if let Some(result) = replace_halfwith_kana(&self.text) {
+            self.text = result;
         }
     }
 }
